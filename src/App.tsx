af9264ec38a0d5d6aa744a057a4dab096b3efa17
kiharito <kihara13611.c7d7@gmail.com>
2024-11-08 18:50:05 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+import logo from './todo-app-logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex justify-between h-14 items-center px-4">
+        <img src={logo} className="size-24 h-6" alt="logo" />
+        <p>kihara</p>
+      </div>
+      <div className='max-w-xl mx-auto'>
+        <div>
+          <h1 className="text-3xl">Register New Task</h1>
+          <input className='rounded h-12 w-96' placeholder='Enter your task'></input>
+          <button>Register</button>
+        </div>
+        <div className='max-w-xl'>
+          <h1 className="text-3xl">Tasks</h1>
+          <ul className='flex'>
+            <li className='w-24 text-center'>Todo</li>
+            <li className='w-24 text-center'>Done</li>
+            <li className='w-24 text-center'>All</li>
+          </ul>
+          <ul>
+            <li className='flex'>
+              <p>ベンチプレス</p><button>Complete</button>
+            </li>
+            <li className='flex'>
+              <p>ベンチプレス</p><button>Complete</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
