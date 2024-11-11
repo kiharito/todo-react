d2879f9defc92fb0902bbd4c4441a7631086289a
kiharito <kihara13611.c7d7@gmail.com>
2024-11-11 14:08:30 +0900

    絶対パスによるimportを可能にする

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { TasksPage } from './tasks';
+import { TasksPage } from 'components/pages/tasks';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
