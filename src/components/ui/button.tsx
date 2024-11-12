5fa5fc624958a8745714a982149d9cb59cb863c4
kiharito <38311245+kiharito@users.noreply.github.com>
2024-11-12 18:32:34 +0900

    ログインページを仮実装 (#5)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,7 +2,7 @@ type ButtonProps = { text: string }
 export function Button(props: ButtonProps): React.JSX.Element {
   return (
     <button
-      className="w-16 box-border border border-solid rounded font-bold shrink-0"
+      className="w-fit h-12 box-border border border-solid rounded px-4 font-bold shrink-0"
       type='submit'
     >{props.text}</button>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { TasksPage } from 'components/pages/tasks';
+import { LoginPage } from 'components/pages/login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <TasksPage />
+    <LoginPage />
   </React.StrictMode>
 );
